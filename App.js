4ab031ae74258b776c8e0d51e4445e49817d50d0
Nícolas Lemes <nicolastloliveira@gmail.com>
2023-02-15 18:32:41 -0300

    starting the project (youtube tutorial)

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { 
+  StyleSheet, 
+  Text, View, 
+  TextInput, 
+  TouchableOpacity, 
+  FlatList
+} from 'react-native';
 
+import {Ionicons, MaterialIcons} from "@expo/vetor-icons"
 export default function App() {
+const [task, setTask] = useState([]);
+const [newTask, setNewTask] = useSate('';)
+
+
   return (
+    <>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.Body}>
+        <FlatList style={style.Flatlist}/>
+      <Text>Hello World!</Text>
+      </View>
+       <View style={styles.Form}>
+      <TextInput 
+      style={styles.Input}
+      placeholderTextColor="999"
+      autoCorrect={true}
+      placeholder="Adicione uma tarefa"
+      maxLength={25}
+      />
+      <TouchableOpacity style={styles.Button}>
+        <Ionicons name='ios-add' size={25} color="#fff"/>
+      </TouchableOpacity>
     </View>
+    </View>
+    </>
   );
 }
 
@@ -14,7 +41,45 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingVertical:20,
+    marginTop20
   },
+  Body: {
+    flex:1
+  }, 
+  Form: {
+    padding:0, 
+    height: 60,
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    paddingTop: 13,
+    borderTopWidth: 1,
+    borderColor: 'eee'
+    },
+
+  Input: {
+    flex: 1,
+    height: 40,
+    backgroundColor: '#eee',
+    borderRadius: 4,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: '#eee'
+  },
+  Button: {
+    height: 40,
+    width: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '1c6cce'
+    borderRadius: 4,
+    marginLeft:10
+  },
+  FlatList: {
+    flex:1,
+    marginTop: 5,
+  }
 });
